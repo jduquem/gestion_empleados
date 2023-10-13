132ce275c7e7defac8850cf6a94c2a4e02620ac9
joaduque <joaduque@bancolombia.com.co>
2023-10-13 18:36:07 -0500

    databases

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gestion_empleados
+
+<!-- Historias de Usuario:
+
+1. Como administrador, quiero poder crear usuarios con roles de empleado y administrador para qeu pueda gestoinar quien tiene acceso al sistema.
+2. Como administrador, quiero poder crear empleados asignadoles clientes y centro de CeCos para que pueda organizar a los empleados segun su asignación.
+3. Como administrador, quiero poder registar la hora de ingreso y salida de los empleados para realizar un seguimiento de las horas trabajadas.
+4. Como administrador, quiero poder ver y actulizar el salario de cada empleado para gestionar sus salarios de manera efectiva. -->
--- gestion_empleados/settings.py
+++ gestion_empleados/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from psycopg2 import Database
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +78,14 @@ WSGI_APPLICATION = 'gestion_empleados.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg',
+        'NAME': 'ccteirbf',
+        'USER': 'ccteirbf',
+        'PASSWORD': '6Ot1hGYGaML_hG09DtWtc64k217Zql_X',
+        'HOST': 'suleiman.db.elephantsql.com',
+        'DATABASES_PORT': '5432'
+
+
     }
 }
 
