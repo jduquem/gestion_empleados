12aee07929a82bd2a9b4aa130ff89c6d2e976750
jduquem <alejandro.duque1104@gmail.com>
2023-10-14 21:56:51 -0500

    templates

--- empleados/urls.py
+++ empleados/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from . import views
+from .views import listar_empleado, empleado_agregar, empleados_actualizar, empleados_eliminar, listar_horarios, horario_agregar, horarios_actualizar, horarios_eliminar 
 
 urlpatterns = [
-    path('listar_empleados/', views.listar_empleado),
-    path('listar_horarios/', views.listar_horarios),
-    path('empleado_agregar/', views.empleado_agregar),
-    path('new/', views.listar_empleado, name='crear_empleado'),
+        # empleados
+    path('listar_empleados/', listar_empleado.as_view(), name='listar_empleados'),
+    path('empleado_agregar/', empleado_agregar.as_view(), name='empleado_agregar'),
+    path('empleados_actualizar/', empleados_actualizar.as_view(), name='empleados_actualizar'),
+    path('empleados_actualizar/<str:identification>', empleados_actualizar.as_view(), name='empleados_actualizar'),
+    path('empleados_eliminar/', empleados_eliminar.as_view(), name='empleados_eliminar'),
+    path('empleados_eliminar/<str:identification>', empleados_eliminar.as_view(), name='empleados_eliminar'),
+
+       # horarios
+    path('listar_horarios/', listar_horarios.as_view(), name='listar_horarios'),
+    path('horario_agregar/', horario_agregar.as_view(), name='horario_agregar'),
+    path('horarios_actualizar/', horarios_actualizar.as_view(), name='horarios_actualizar'),
+    path('horarios_actualizar/<str:identification>', horarios_actualizar.as_view(), name='horarios_actualizar'),
+    path('horarios_eliminar/', horarios_eliminar.as_view(), name='horarios_eliminar'),
+    path('horarios_eliminar/<str:identification>', horarios_eliminar.as_view(), name='horarios_eliminar'),
+
  ]
--- empleados/views.py
+++ empleados/views.py
@@ -2,52 +2,197 @@ from django.shortcuts import render, redirect, HttpResponseRedirect
 from .models import Employee, EmployeeShift
 from django.contrib import messages
 from django.urls import reverse
+from django.views import View
+import datetime
+
+### empleados ###
+# Clase para listar los empleados
+class listar_empleado(View):
+    template_name = 'empleado.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            empleados = Employee.objects.all()
+            return render(request, self.template_name, {'empleados':empleados})
+        except:
+            return render(request, self.template_name)
+
+# Clase para agregar los empleados
+class empleado_agregar(View):
+    template_name = 'empleado_agregar.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = Employee.objects.all()
+            return render(request, self.template_name, {'queryset':queryset})
+        except:
+            return render(request, self.template_name)    
+    
+    def post(self, request, *args, **kwargs):
+        queryset = Employee.objects.all()
+        for e in queryset:
+            if e == request.POST['identification']:
+                messages.warning(request, 'El empleado con cedula {} ya se encuentra creado'.format(request.POST['identification']))
+                return render(request, 'empleado_agregar.html')
+            else:
+                try:
+                    identification=request.POST['identification']
+                    name=request.POST['name']
+                    gender=request.POST['gender']
+                    email=request.POST['email']
+                    numberphone=request.POST['numberphone']
+                    salary=request.POST['salary']
+                    city=request.POST['city']
+                    Employee.objects.create(identification = identification, name=name, gender=gender, numberphone=numberphone, email=email, salary=salary, city=city)
+                    messages.success(request, 'Se ha creado el empleado con identification {}'.format(identification))
+                    return HttpResponseRedirect(reverse('empleado'))
+                except:
+                    return render(request, self.template_name,)
+        return render(request, self.template_name,)
+
+# Clase para modificar los empleados
+class empleados_actualizar(View):
+    template_name = 'empleados_actualizar.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = Employee.objects.all()
+            return render(request, self.template_name, {'queryset':queryset})
+        except:
+            return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            identification =request.POST['identification']
+            name=request.POST['name']
+            gender=request.POST['gender']
+            email=request.POST['email']
+            numberphone=request.POST['numberphone']
+            salary=request.POST['salary']
+            city=request.POST['city']
+
+            empleados = Employee.objects.get(identification=kwargs['identification'])
+            empleados.name = name
+            empleados.gender= gender
+            empleados.email= email
+            empleados.numberphone=numberphone
+            empleados.salary= salary
+            empleados.city = city
+            empleados.save()
+            return HttpResponseRedirect(reverse('empleado',))
+        except:
+            return render(request, self.template_name)
+
+# Clase para eliminar los empleados
+class empleados_eliminar(View):
+    template_name = 'empleados_eliminar.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            empleados = Employee.objects.get(indentification=args)
+            empleados.delete()
+            messages.warning(request, 'El empleado con cedula {} se elimino correctamente'.format(args))
+            return render(request, self.template_name)
+        except:
+            return render(request, self.template_name)
 
 
-def listar_empleado(request):
-    empleados = Employee.objects.all()
-    return render(request, 'empleado.html', {'empleados': empleados})
 
-def empleado_agregar(request):
-    queryset = Employee.objects.all()
-    for e in queryset:
-        if e == request.POST['identification']:
-            messages.warning(request, 'El empleado con cedula {} ya se encuentra creado'.format(request.POST['identification']))
-            return render(request, 'empleado.html')
-        else:
-            try:
-                identification=request.POST['identification'].upper()
-                name=request.POST['name']
-                gender=request.POST['gender']
-                email=request.POST['email']
-                numberphone=request.POST['numberphone']
-                salary=request.POST['salary']
-                city=request.POST['city']
-                Employee.objects.create(identification = identification, name=name, gender=gender, numberphone=numberphone, email=email, salary=salary, city=city)
-                messages.success(request, 'Se ha creado el empleado con identification {}'.format(identification))
-                return HttpResponseRedirect(reverse('empleado'))
-            except:
-                return render(request, 'empleado.html')
-    return render(request, 'empleado.html')
+### horarios ###
+# Clase para listar los horarios
+class listar_horarios(View):
+    template_name = 'horario.html'
 
-def listar_horarios(request):
-    horarios = EmployeeShift.objects.all()
-    return render(request, 'horario.html', {'horarios': horarios})
+    def get(self, request, *args, **kwargs):
+        try:
+            horarios = EmployeeShift.objects.all()
+            return render(request, self.template_name, {'horarios':horarios})
+        except:
+            return render(request, self.template_name)
+
+# Clase para agregar los horarios
+class horario_agregar(View):
+    template_name = 'horario_agregar.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = EmployeeShift.objects.all()
+            return render(request, self.template_name, {'queryset':queryset})
+        except:
+            return render(request, self.template_name)    
+    
+    def post(self, request, *args, **kwargs):
+        queryset = EmployeeShift.objects.all()
+        for e in queryset:
+            if e == request.POST['employee']:
+                messages.warning(request, 'El horario para el empleado {} ya se encuentra creado'.format(request.POST['employee']))
+                return render(request, 'horario_agregar.html')
+            else:
+                try:
+                    employee=request.POST['employee']
+                    date_reg=request.POST['date_reg']
+                    entry_time=request.POST['entry_time']
+                    departure_time=request.POST['departure_time']
+                    h_init = datetime.strptime(entry_time, "%H:%M:%S")
+                    h_end = datetime.strptime(departure_time, "%H:%M:%S")
+                    diferencia = h_end - h_init
+                    # holiday = festivos(date_reg)
+                    total_hours= diferencia.total_seconds() / 3600 
+                    EmployeeShift.objects.create(employee = employee, date_reg=date_reg, entry_time=entry_time, departure_time=departure_time, holiday=holiday, total_hours=total_hours)
+                    messages.success(request, 'Se ha creado el horario para el empleado {}'.format(employee))
+                    return HttpResponseRedirect(reverse('horario'))
+                except:
+                    return render(request, self.template_name,)
+        return render(request, self.template_name,)
+
+# Clase para modificar los horarios
+class horarios_actualizar(View):
+    template_name = 'horarios_actualizar.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            queryset = EmployeeShift.objects.all()
+            return render(request, self.template_name, {'queryset':queryset})
+        except:
+            return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            employee=request.POST['employee']
+            date_reg=request.POST['date_reg']
+            entry_time=request.POST['entry_time']
+            departure_time=request.POST['departure_time']
+            h_init = datetime.strptime(entry_time, "%H:%M:%S")
+            h_end = datetime.strptime(departure_time, "%H:%M:%S")
+            diferencia = h_end - h_init
+            # holiday = festivos(date_reg)
+            total_hours= diferencia.total_seconds() / 3600 
+
+            empleados = EmployeeShift.objects.get(employee=kwargs['employee'])
+            empleados.date_reg = date_reg
+            empleados.entry_time= entry_time
+            empleados.departure_time= departure_time
+            empleados.total_hours=total_hours
+            empleados.save()
+            return HttpResponseRedirect(reverse('horario',))
+        except:
+            return render(request, self.template_name)
+
+# Clase para eliminar los horarios
+class horarios_eliminar(View):
+    template_name = 'horarios_eliminar.html'
+
+    def get(self, request, *args, **kwargs):
+        try:
+            horarios = EmployeeShift.objects.get(employee=args)
+            horarios.delete()
+            messages.warning(request, 'El horario para el empleado {} se elimino correctamente'.format(args))
+            return render(request, self.template_name)
+        except:
+            return render(request, self.template_name)
 
 
 
-# def crear_empleado(request):
-#     identification = request.POST['identification']
-#     Nombre = request.POST['Nombre']
-#     Correo = request.POST['Correo']
-#     Numero_telefonico = request.POST['Numero_telefonico']
-#     Salario = request.POST['Salario']
-#     Ciudad_de_recidencia = request.POST['Ciudad_de_recidencia']
-#     empleado = Employee(identification=identification, Nombre=Nombre, Correo=Correo, 
-#                         Numero_telefonico = Numero_telefonico, Salario = Salario, 
-#                         Ciudad_de_recidencia = Ciudad_de_recidencia )
-#     empleado.save()
-#     return redirect("empleado.html")
 
 
 
@@ -61,68 +206,30 @@ def listar_horarios(request):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Definir la URL de la API y los parámetros
-# api_url = "https://date.nager.at/api/v3/publicholidays/2023/CO"
-# country_code = "CO"  # Código de país (ejemplo: Colombia)
-
-# # Parámetros de la solicitud
-# params = {
-#     "CountryCode": country_code,
-#     "Year": 2023  # Puedes ajustar el año
-# }
-
-# # Realizar la solicitud a la API
-# response = requests.get(api_url, params=params)
-
-# # Verificar si la solicitud fue exitosa
-# if response.status_code == 200:
-#     # La solicitud fue exitosa
-#     holidays = response.json()
-#     for holiday in holidays:
-#         print(f"{holiday['date']}")
-
-# else:
-#     # La solicitud no fue exitosa
-#     print(f"Error {response.status_code}: No se pudo obtener la información de días festivos.")
-
+# def festivos(fecha):
+#     # Definir la URL de la API y los parámetros
+#     api_url = "https://date.nager.at/api/v3/publicholidays/2023/CO"
+#     country_code = "CO"  # Código de país (ejemplo: Colombia)
+#     # Parámetros de la solicitud
+#     params = {
+#         "CountryCode": country_code,
+#         "Year": 2023  # Puedes ajustar el año
+#     }
+#     # Realizar la solicitud a la API
+#     response = requests.get(api_url, params=params)
+#     # Verificar si la solicitud fue exitosa
+#     if response.status_code == 200:
+#         # La solicitud fue exitosa
+#         holidays = response.json()
+#         for holiday in holidays:
+#             print(f"{holiday['date']}")
+#     else:
+#         # La solicitud no fue exitosa
+#         print(f"Error {response.status_code}: No se pudo obtener la información de días festivos.")
+#     return 
 
 # # Define la fecha que quieres verificar
+
 # fecha = datetime.date(2023, 10, 16)  # Formato: año, mes, día (2023-10-15)
 
 # # Verifica si la fecha es un domingo (0 = lunes, 6 = domingo)
