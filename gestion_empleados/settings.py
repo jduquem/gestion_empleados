d168a37d4349ff8e722b3b10f6158d9b0b86d1cc
alejo duque <alejandro.duque1104@gmail.com>
2023-10-18 09:38:04 -0500

    url settings

--- gestion_empleados/settings.py
+++ gestion_empleados/settings.py
@@ -100,6 +100,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 # Password validation
