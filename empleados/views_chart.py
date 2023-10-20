302ce6582757d3c492e1edc5df4b71ef04f11112
jduquem <alejandro.duque1104@gmail.com>
2023-10-20 02:33:45 -0500

    Graficos

--- empleados/views_chart.py
+++ empleados/views_chart.py
@@ -10,98 +10,45 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .utils import arreglo
 import json
 from django.db.models.functions import TruncMonth
+from django.db.models import Sum, Count
 
 class Chart(LoginRequiredMixin, View):
     template_name = 'empleado/empleado_eliminar.html'
 
     def get(self, request):
-        context = {'data':100}
-        return render(request, 'nomina/chart.html', context)        
+        # context = {'data':100}
 
-
-class SalaryAverageByGender(View):
-    template_name = 'nomina/chart.html'
-
-    def get(self, request, *args, **kwargs):
+        # promedio salario por genero
         male_average_salary = Employee.objects.filter(gender='M').aggregate(avg_salary=Avg('salary'))['avg_salary']
         female_average_salary = Employee.objects.filter(gender='F').aggregate(avg_salary=Avg('salary'))['avg_salary']
         other_average_salary = Employee.objects.filter(gender='O').aggregate(avg_salary=Avg('salary'))['avg_salary']
 
-        context = {
-            'genders': ['Masculino', 'Femenino'],
-            'average_salaries': [male_average_salary, female_average_salary, other_average_salary],
-        }
+        # SalaryTotalByMonth
+        monthly_salary_data = EmployeeShift.objects.annotate(month=TruncMonth('date_reg')).values('month').annotate(total_salary=Sum('employee__salary')).order_by('month')
+        campos = [item['month'].strftime('%B %Y') for item in monthly_salary_data]
+        valor = [item['total_salary'] for item in monthly_salary_data]
 
-        return render(request, 'nomina/chart.html', context)
-    
-class SalaryTotalByMonth(View):
-    template_name = 'nomina/chart.html'
+        
+        # EmployeesByMonth
+        # monthly_employees_data = Employee.objects.annotate(month=TruncMonth('date_reg')).values('month').annotate(total_employees=Count('id')).order_by('month')
+        # labels = [item['month'].strftime('%B %Y') for item in monthly_employees_data]
+        # data = [item['total_employees'] for item in monthly_employees_data]
 
-    def get(self, request, *args, **kwargs):
-        monthly_salary_data = EmployeeShift.objects.annotate(
-            month=TruncMonth('date_reg')
-        ).values('month').annotate(
-            total_salary=Sum('employee__salary')
-        ).order_by('month')
-
-        labels = [item['month'].strftime('%B %Y') for item in monthly_salary_data]
-        data = [item['total_salary'] for item in monthly_salary_data]
-
-        context = {
-            'labels': labels,
-            'data': data,
-        }
-        return render(request, self.template_name, context)
-
-class EmployeesByMonth(View):
-    template_name = 'nomina/chart.html'
-
-    def get(self, request, *args, **kwargs):
-        try:
-            # Obtener datos de la cantidad de empleados por mes
-            monthly_employees_data = Employee.objects.annotate(
-                month=TruncMonth('date_joined')
-            ).values('month').annotate(
-                total_employees=Count('id')
-            ).order_by('month')
-
-            labels = [item['month'].strftime('%B %Y') for item in monthly_employees_data]
-            data = [item['total_employees'] for item in monthly_employees_data]
-
-            return render(request, self.template_name, {'labels': labels, 'data': data})
-        except Exception as e:
-            print(str(e))
-            return render(request, self.template_name)
-
-
-class TotalSalary(View):
-    template_name = 'nomina/chart.html'
-
-    def get(self, request, *args, **kwargs):
         total_salary = Employee.objects.aggregate(total_salary=Sum('salary'))['total_salary']
 
+        # AverageHoursByMonth
+        monthly_hours_data = EmployeeShift.objects.annotate(month=TruncMonth('date_reg')).values('month').annotate(avg_hours=Avg('total_hours')).order_by('month')
+        labels = [item['month'].strftime('%B %Y') for item in monthly_hours_data]
+        data = [item['avg_hours'] for item in monthly_hours_data]
+        
         context = {
+            'genders': {'Masculino', 'Femenino', 'Otro'},
+            'average_salaries': {male_average_salary, female_average_salary, other_average_salary},
+            'campos':campos,
+            'valor': valor,
             'total_salary': total_salary,
+            'labels':labels,
+            'data': data
         }
 
-        return render(request, self.template_name, context)
-    
-
-class AverageHoursByMonth(View):
-    template_name = 'nomina/chart.html'
-
-    def get(self, request, *args, **kwargs):
-        try:
-            # Obtener datos del promedio de horas por mes
-            monthly_hours_data = EmployeeShift.objects.annotate(
-                month=TruncMonth('date_reg')
-            ).values('month').annotate(
-                avg_hours=Avg('total_hours')
-            ).order_by('month')
-
-            labels = [item['month'].strftime('%B %Y') for item in monthly_hours_data]
-            data = [item['avg_hours'] for item in monthly_hours_data]
-            return render(request, self.template_name, {'labels': labels, 'data': data})
-        except Exception as e:
-            print(str(e))
-            return render(request, self.template_name)
+        return render(request, 'nomina/chart.html', {'context': context})        
