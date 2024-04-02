127e2c4ea7a42278ac851b3171aa263bf7169d7e
alejo duque <alejandro.duque1104@gmail.com>
2024-04-01 21:24:41 -0500

    update

--- empleados/views_chart.py
+++ empleados/views_chart.py
@@ -1,23 +1,23 @@
 from .models import Employee
-from shift.models import EmployeeShift
+from shift.models EmployeeShift
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
 from django.views.generic import View
-from django.http import JsonResponse
 from django.db.models import Avg
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .utils import arreglo
-import json
 from django.db.models.functions import TruncMonth
 from django.db.models import Sum, Count
 
 class Chart(LoginRequiredMixin, View):
-    template_name = 'empleado/empleado_eliminar.html'
 
     def get(self, request):
-        # context = {'data':100}
+
+        # cantidad de empleados
+        count_employees = Employee.objects.count()
+
+        #salario total
+        total_salary = Employee.objects.aggregate(total_salary=Sum('salary'))['total_salary']
 
         # promedio salario por genero
         male_average_salary = Employee.objects.filter(gender='M').aggregate(avg_salary=Avg('salary'))['avg_salary']
@@ -26,8 +26,8 @@ class Chart(LoginRequiredMixin, View):
 
         # SalaryTotalByMonth
         monthly_salary_data = EmployeeShift.objects.annotate(month=TruncMonth('date_reg')).values('month').annotate(total_salary=Sum('employee__salary')).order_by('month')
-        campos = [item['month'].strftime('%B %Y') for item in monthly_salary_data]
-        valor = [item['total_salary'] for item in monthly_salary_data]
+        months_salary_per_month = [item['month'].strftime('%B %Y') for item in monthly_salary_data]
+        total_salary_per_month = [item['total_salary'] for item in monthly_salary_data]
 
         
         # EmployeesByMonth
@@ -35,21 +35,38 @@ class Chart(LoginRequiredMixin, View):
         # labels = [item['month'].strftime('%B %Y') for item in monthly_employees_data]
         # data = [item['total_employees'] for item in monthly_employees_data]
 
-        total_salary = Employee.objects.aggregate(total_salary=Sum('salary'))['total_salary']
-
         # AverageHoursByMonth
         monthly_hours_data = EmployeeShift.objects.annotate(month=TruncMonth('date_reg')).values('month').annotate(avg_hours=Avg('total_hours')).order_by('month')
-        labels = [item['month'].strftime('%B %Y') for item in monthly_hours_data]
-        data = [item['avg_hours'] for item in monthly_hours_data]
+        months_avg_salary_per_month = [item['month'].strftime('%B %Y') for item in monthly_hours_data]
+        total_avg_salary_per_month = [item['avg_hours'] for item in monthly_hours_data]
         
         context = {
-            'genders': {'Masculino', 'Femenino', 'Otro'},
-            'average_salaries': {male_average_salary, female_average_salary, other_average_salary},
-            'campos':campos,
-            'valor': valor,
+            'count_employees':count_employees,
             'total_salary': total_salary,
-            'labels':labels,
-            'data': data
+
+            'genders': {'Masculino', 'Femenino', 'Otro'},
+            'average_salaries_gender': {male_average_salary, female_average_salary, other_average_salary},
+            
+            'months_salary_per_month':months_salary_per_month,
+            'total_salary_per_month': total_salary_per_month,
+            
+            'months_avg_salary_per_month':months_avg_salary_per_month,
+            'total_avg_salary_per_month': total_avg_salary_per_month
         }
 
         return render(request, 'nomina/chart.html', {'context': context})        
+
+
+class Dashboard():
+
+    def a():
+        return 0
+
+    def a():
+        return 0
+
+    def a():
+        return 0
+
+    def a():
+        return 0
